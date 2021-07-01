6272cb6fe267f8d0e108fabe05ac2d8b512a87fb
nicktesh <56547918+nicktesh@users.noreply.github.com>
2021-07-01 16:32:38 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//SMOOTH SCROLLING
+// SMOOTH SCROLLING
 $(document).ready(function () {
   // Add smooth scrolling to all links
   $("a").on("click", function (event) {
@@ -26,6 +26,28 @@ $(document).ready(function () {
   });
 });
 // END OF SMOOTH SCROLLING
+
+// SCROLL TO TOP
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $("#back-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
+// END OF SCROLL TO TOP
 
 // GRAVITY SWITCH
 let gravitySwitch = document.getElementById("gravitySwitch");
